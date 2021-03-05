c7196a693d62b4ce0c400d1c562d9b0952e34181
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-04 23:51:13 -0500

    added data parsing and listener code blocks

--- server.js
+++ server.js
@@ -8,3 +8,11 @@ const { json } = require("express");
 // Ser up of express app
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+// express data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// server listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
