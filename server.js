d180253fafff5ccda90b38fbe5b24b4cf3cf4203
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-04 23:49:11 -0500

    set up express

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ const jsondb = require("./db/db.json");
 const { v4: uuidv4 } = require('uuid');
 const { json } = require("express");
 
+// Ser up of express app
+var app = express();
+var PORT = process.env.PORT || 3000;
