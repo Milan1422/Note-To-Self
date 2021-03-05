be8ca428085d6d98dd00d24f4d115e03d602cc8b
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-04 23:58:43 -0500

    added routes

--- server.js
+++ server.js
@@ -14,5 +14,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// routes 
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function(req,res){
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function(req,res){
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+
 // server listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
